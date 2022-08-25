023169b1c46d5b4a1e603220b92a777c35906f21
Owanikin <oderindeife@gmail.com>
2022-08-25 14:24:40 +0100

    mocking contract: MockV3Aggregtor.sol

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -3,6 +3,7 @@
 //  }
 
 const { network } = require("hardhat")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
 
 //  module.exports.default = deployFunc
 
@@ -11,7 +12,28 @@ module.exports = async({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
+    
+    // const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    
+    let ethUsdPriceFeedAddress
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+
+
+    // if the contract doesn't exist, we deploy a minimal version for our local testing
 
     // Well what happens when we want to change chains?
     // When going for localhost or hardhat network we want to use a mock
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [ethUsdPriceFeedAddress], // put price feed address
+        log: true,
+    })
+    log("---------------------------")
 }
+
+module.exports.tags = ["all", "fundme"]
--- hardhat.config.js
+++ hardhat.config.js
@@ -3,5 +3,22 @@ require("hardhat-deploy")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337
+    }
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0
+    }
+  },
+  // solidity: "0.8.9",
+  solidity: {
+    compilers: [
+      {version: "0.8.9"},
+      {version: "0.6.6"}
+    ]
+  }
 };
